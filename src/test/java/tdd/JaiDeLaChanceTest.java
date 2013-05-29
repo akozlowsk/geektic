c355bb40c167a687a162fdfe7fcdea897101c34f
adam <adam.kozlowski@hotmail.fr>
2013-05-29 16:00:24 +0200

    ajout d'une premiere classe

---
+++ src/test/java/tdd/JaiDeLaChanceTest.java
@@ -0,0 +1,15 @@
+package tdd;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JaiDeLaChanceTest {	
+	
+	
+	@Test
+	public void retourneUnSeulGeek(){
+		assertEquals(1,1);
+	}
+	
+}
